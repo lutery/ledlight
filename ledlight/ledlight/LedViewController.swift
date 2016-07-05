613ea77dbc6f49b3ca1e70704fad53c8938f22b6
杨辉 <453413516@qq.com>
2016-07-05 18:10:06 +0800

    已经创建初步的工程，剩下就是创建新的控件

---
+++ ledlight/ledlight/LedViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LedViewController.swift
+//  ledlight
+//
+//  Created by 杨辉 on 16/7/5.
+//  Copyright © 2016年 杨辉. All rights reserved.
+//
+
+import UIKit
+
+class LedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+//        self.navigationController?.navigationBar.backgroundColor = UIColor.black();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default);
+        self.navigationController?.navigationBar.shadowImage = UIImage();
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated);
+        
+        self.navigationController?.navigationBar.setBackgroundImage(nil, for: UIBarMetrics.default);
+        self.navigationController?.navigationBar.shadowImage = nil;
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ledlight/ledlight/ViewController.swift
+++ ledlight/ledlight/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mLedText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onClickSure(_ sender: AnyObject) {
+        let content = mLedText.text;
+        
+        if content?.characters.count == 0 || content == nil {
+            let alert = UIAlertController(title: "提示", message: "没有输入显示的文字", preferredStyle: .alert);
+            alert.addAction(UIAlertAction(title: "确定", style: .default, handler: nil));
+            self.present(alert, animated: true, completion: nil);
+            return;
+        }
+        
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil);
+        let ledViewController = mainStoryboard.instantiateViewController(withIdentifier: "ledVC");
+        self.navigationController?.pushViewController(ledViewController, animated: true);
+    }
 
+    @IBAction func onTextDone(_ sender: AnyObject) {
+        print("onTextDone");
+    }
 }
 
