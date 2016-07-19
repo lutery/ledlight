a6a29cdeb0e538963602fed682d4133b42fe6504
ch4534 <453413516@qq.com>
2016-07-19 15:22:23 +0800

    增加了小亮点的绘制

--- ledlight/ledlight/LedView.swift
+++ ledlight/ledlight/LedView.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class LedView: UIView {
+    
+    let radius = 16;
+    let halfRadius = 8;
+    var ledColor = UIColor.white();
+    var bgColor = UIColor.black();
+    var circularPos:[CGPoint]? = nil;
+    var xPoints = 0;
+    var yPoints = 0;
 
     /*
     // Only override draw() if you perform custom drawing.
@@ -17,10 +25,44 @@ class LedView: UIView {
         // Drawing code
     }
     */
+    
+    override init(frame:CGRect){
+        super.init(frame: frame);
+        
+//        print(frame);
+        let mainScreenRect = UIScreen.main().bounds;
+        print(mainScreenRect);
+        
+        xPoints = Int(mainScreenRect.width / CGFloat(radius * 2));
+        yPoints = Int(mainScreenRect.height / CGFloat(radius * 2));
+        
+        let startPoint = CGPoint(x: 16, y: 16);
+        circularPos = [CGPoint]();
+        
+        for i in 0...yPoints {
+            for j in 0...xPoints {
+                let curPoint = CGPoint(x: Int(startPoint.x + CGFloat(j * radius * 2)), y: Int(startPoint.y + CGFloat(i * radius * 2)));
+                circularPos?.append(curPoint);
+            }
+        }
+    }
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func draw(_ rect: CGRect) {
+        super.draw(rect);
+        print("ledView draw \(rect)");
+        
         let context = UIGraphicsGetCurrentContext();
-        context?.setFillColor(UIColor.black().cgColor);
+        context?.setFillColor(bgColor.cgColor);
         context?.fill(rect);
+        
+        context?.setFillColor(ledColor.cgColor);
+        for curPos in circularPos! {
+            context?.fillEllipse(in: CGRect(x: Int(curPos.x - CGFloat(halfRadius)), y: Int(curPos.y - CGFloat(halfRadius)), width: 16, height: 16));
+        }
     }
 }
--- ledlight/ledlightUITests/ledlightUITests.swift
+++ ledlight/ledlightUITests/ledlightUITests.swift
@@ -31,6 +31,16 @@ class ledlightUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication();
+        let displayText = app.textFields["displayText"];
+        displayText.tap();
+        displayText.typeText("这是一个测试");
+//        typeSomethingTextField.
+        
+        let sureBtn = app.buttons["sureBtn"];
+        sureBtn.tap();
+        
+        print("displayText");
     }
     
 }
